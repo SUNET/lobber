e09c7f411165527010181e9cdb9f28bd3b688fcf
Linus Nordberg <linus@nordushare-dev.nordu.net>
2009-11-16 00:04:15 +0100

    * src/lobber/share/forms.py: Don't require 'published' to be checked (kind of pointless).  See http://docs.djangoproject.com/en/dev/ref/forms/fields/#booleanfield for the full story.

--- src/lobber/share/forms.py
+++ src/lobber/share/forms.py
@@ -3,5 +3,5 @@ from django import forms
 class UploadTorrentForm(forms.Form):
     file = forms.FileField()
     name = forms.CharField(max_length=128)
-    published = forms.BooleanField(initial=True)
+    published = forms.BooleanField(initial=True, required=False)
     expires = forms.DateTimeField()
