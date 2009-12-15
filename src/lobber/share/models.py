b1ccd30dafe620baeec58ea8ba04e1bb1d70d840
Linus Nordberg <linus@nordberg.se>
2009-12-15 15:55:05 +0100

    Data model #3, take one.

--- src/lobber/share/models.py
+++ src/lobber/share/models.py
@@ -1,32 +1,46 @@
 from django.db import models
+# http://docs.djangoproject.com/en/dev/topics/auth/#module-django.contrib.auth
 from django.contrib.auth.models import User
 
-# Create your models here.
-
-#class User(models.Model):
-#    pass
-# http://docs.djangoproject.com/en/dev/topics/auth/#module-django.contrib.auth
-
 class Torrent(models.Model):
-    #owner = models.ForeignKey(User)
-    name = models.CharField(max_length=128, blank=True)
+    owner = models.ForeignKey(User)
+    name = models.CharField(max_length=256, blank=True)
+    #tags = models.ManyToManyFields(Tag)
+
     data = models.FileField(upload_to='torrents') # Dir in MEDIA_ROOT.
     hashval = models.CharField(max_length=40)
-    def __unicode__(self):
-        return '%s "%s"' % (self.hashval, self.name)
-
-class Handle(models.Model):
-    """A handle is supposed to be published in some way, enabling
-    other users to get to the torrent.
-    """
-    torrent = models.ForeignKey(Torrent)
-    name = models.CharField(max_length=1024) # Makes up the URL.
-    published = models.BooleanField(default=True)
-    creation = models.DateTimeField(auto_now_add=True)
-    expiration = models.DateTimeField()
 
     def __unicode__(self):
-        return self.name
+        return '%s "%s" (%s)' % (self.hashval, self.name, self.owner.username)
 
-    def get_absolute_url(self):
-        return '/%s/' % self.id
+class Tag(models.Model):
+    owner = models.ForeignKey(User)
+    name = models.CharField(max_length=256, blank=True)
+    torrents = models.ManyToManyFields(Torrent)
+
+class Key(models.Model):
+    owner = models.ForeignKey(User)
+    secret = models.CharField(max_length=256, primary_key=True)
+    acl = models.ForeignKey(ACL)
+    comment = models.CharField(max_length=64) # F.ex. user (for audit trail).
+
+class ACL(models.Model):
+    acs = models.TextField()    
+
+
+# class Handle(models.Model):
+#     """A handle is supposed to be published in some way, enabling
+#     other users to get to the torrent.
+#     """
+#     torrent = models.ForeignKey(Torrent)
+#     name = models.CharField(max_length=1024) # Makes up the URL.
+#     published = models.BooleanField(default=True)
+#     creation = models.DateTimeField(auto_now_add=True)
+#     expiration = models.DateTimeField()
+#
+#     def __unicode__(self):
+#         return self.name
+#
+#     def get_absolute_url(self):
+#         return '/%s/' % self.id
+
