4074acb2b0f44ae1a50dd351428dda214da45f6c
Linus Nordberg <linus@nordberg.se>
2010-02-24 13:58:33 +0100

    Collapse the forms to one again.

--- src/lobber/share/forms.py
+++ src/lobber/share/forms.py
@@ -3,10 +3,5 @@ from django import forms
 class UploadForm(forms.Form):
     name = forms.CharField(max_length=128, label="Description")
     expires = forms.DateTimeField(label="Expire date")
-    published = forms.BooleanField(initial=True, required=False, label="Listed")
-
-class UploadTorrentForm(UploadForm):
+    published = forms.BooleanField(initial=True, required=False, label="Publicize")
     file = forms.FileField(label="Torrent file")
-
-class UploadAppletForm(UploadForm):
-    file = forms.FileField(label="foo bar bletch")
