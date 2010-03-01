c00492541d9729da84283ceb6b485578a00210b4
www-data <www-data@nordushare-dev.nordu.net>
2010-03-01 14:17:51 +0100

    class ACL is gone; fix typo in share/urls.py.

--- src/lobber/share/admin.py
+++ src/lobber/share/admin.py
@@ -4,4 +4,3 @@ from django.contrib import admin
 admin.site.register(Torrent)
 admin.site.register(Tag)
 admin.site.register(Key)
-admin.site.register(ACL)
--- src/lobber/share/views.py
+++ src/lobber/share/views.py
@@ -9,7 +9,7 @@ from django.contrib.auth.decorators import login_required
 
 from settings import BASE_DIR, MEDIA_ROOT, LOGIN_URL, ANNOUNCE_URL, NORDUSHARE_URL, BASE_UI_URL
 from forms import UploadForm
-from lobber.share.models import Torrent, Tag, Key, ACL
+from lobber.share.models import Torrent, Tag, Key
 
 ####################
 # Helper functions. FIXME: Move to some other file.
