a7e768095538b0f48b14b69feae16fb523e9980e
Linus Nordberg <linus@nordberg.se>
2010-03-01 14:01:48 +0100

    * src/lobber/share/models.py: Remove class ACL -- let ACL simply be a TextField in class Torrent. (Tag): Revive and add it as a ManyToManyField in Torrent.

--- src/lobber/share/models.py
+++ src/lobber/share/models.py
@@ -2,18 +2,6 @@ from django.db import models
 # http://docs.djangoproject.com/en/dev/topics/auth/#module-django.contrib.auth
 from django.contrib.auth.models import User
 
-class ACL(models.Model):
-    aces = models.TextField()           # Space separated ace's.
-
-    def auth(self, username, perm):
-        for ace in self.aces.split(' '):
-            if ace.startswith('user:%s' % username):
-                if ace.endswith('#w'):  # Write permission == all.
-                    return True
-                if ace.endswith('#%c' % perm):
-                    return True
-        return False
-
 class Torrent(models.Model):
     name = models.CharField(max_length=256, blank=True)
     creation = models.DateTimeField(auto_now_add=True)
@@ -21,20 +9,28 @@ class Torrent(models.Model):
     expiration = models.DateTimeField()
     data = models.FileField(upload_to='torrents') # upload_to: directory in MEDIA_ROOT.
     hashval = models.CharField(max_length=40)
-    acl = models.OneToOneField(ACL)
-#    tags = models.ManyToManyFields(Tag)
+    acl = models.TextField()
+    tags = models.ManyToManyField('Tag')
 
     def __unicode__(self):
         return '%s "%s" (%s)' % (self.hashval, self.name, self.owner.username)
 
-#class Tag(models.Model):
-#    torrents = models.ManyToManyField(Torrent)
-#    value = models.CharField(max_length=256, blank=True)
+    def auth(self, username, perm):
+        for ace in self.acl.split(' '):
+            if ace.startswith('user:%s' % username):
+                if ace.endswith('#w'):  # Write permission == all.
+                    return True
+                if ace.endswith('#%c' % perm):
+                    return True
+        return False
+
+class Tag(models.Model):
+    value = models.CharField(max_length=64, blank=True, primary_key=True)
 
 class Key(models.Model):
     secret = models.CharField(max_length=64, primary_key=True)
     entitlements = models.TextField()         # Space separated entls.
-    urlfilter = models.TextField()            # Space separated regexps.
+    urlfilter = models.TextField()            # Space separated simplified regexps.
     comment = models.CharField(max_length=64) # F.ex. user (for audit trail).
 
 class UserProfile(models.Model):
