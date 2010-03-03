0919a16e7ddba682c90d98d496efdd1407683448
Linus Nordberg <linus@nordushare-dev.nordu.net>
2010-03-03 23:36:37 +0100

    * src/lobber/middleware.py: Try to get key middleware working.  Still not really.

--- src/lobber/middleware.py
+++ src/lobber/middleware.py
@@ -25,7 +25,7 @@ class KeyMiddleware(object):
                 " before the KeyMiddleware class.")
         # Look for 'lkey' in query part of URL.
         secret = request.REQUEST.get('lkey', None)
-        if not secret:
+        if secret is None:
             return
 
         # TODO: Sanitize secret (a.k.a. un-bobby-tablify it).
@@ -42,7 +42,8 @@ class KeyMiddleware(object):
             logger.error("%s: user profile not found" % username)
             return
 
-        if profile.expiration_date <= dt.now():
+        if profile.expiration_date is not None and \
+               profile.expiration_date <= dt.now():
             logger.info("%s: key expired" % secret)
             return
 
@@ -57,9 +58,28 @@ class KeyMiddleware(object):
                         % (username, cmd, profile.urlfilter))
             return
 
-        profile.displayname = username
-        request.user = user
-        auth.login(request, user)
+        if request.user.is_authenticated():
+            if request.user.username == self.clean_username(username, request):
+                logger.info("user %s already authenticated" % username)
+                return                  # All ok.
+        xxx = auth.authenticate(remote_user=username)
+        logger.info("authenticated %s, got %s" % (username, xxx))
+        if xxx:
+            profile.displayname = username
+            request.user = xxx
+            auth.login(request, xxx)
         
     # TODO: Add a configure_user(user) method?
     
+    def clean_username(self, username, request):
+        """
+        Allows the backend to clean the username, if the backend defines a
+        clean_username method.
+        """
+        backend_str = request.session[auth.BACKEND_SESSION_KEY]
+        backend = auth.load_backend(backend_str)
+        try:
+            username = backend.clean_username(username)
+        except AttributeError: # Backend has no clean_username method.
+            pass
+        return username
