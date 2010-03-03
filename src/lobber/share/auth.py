0a82984a3a9ab010a3a07a4db18db6119fa43ad5
Linus Nordberg <linus@nordushare-dev.nordu.net>
2010-03-03 16:46:33 +0100

    * src/lobber/share/auth.py (login_federated): Protect against key users logging in via IDP and getting url filter '.*' in case their filter wasn't found.

--- src/lobber/share/auth.py
+++ src/lobber/share/auth.py
@@ -39,64 +39,78 @@ def login(request):
     return render_to_response('share/login.html',{'user': request.user,'next': request.REQUEST.get("next")});
 
 def login_federated(request):
-    if request.user.is_authenticated():
-        update = False
+    def logfailure():
+        logger.warning("Failed federated login for user %s from %s" %
+                       (username, req_meta(request, "REMOTE_ADDR")))
+    
+    # Authenticated?
+    if not request.user.is_authenticated():
+        logfailure()
+        return
+    username = request.user.username
+    
+    # Key users ('key:<secret>') must already have a profile -- for
+    # url filtering.
+    try:
+        profile = request.user.profile.get()
+    except ObjectDoesNotExist:
+        if username.startswith('key:'):
+            logger.error("Key user %s doesn't have a profile" % username)
+            logfailure()
+            return
 
-        for attrib_name, meta_name in (("first_name", "HTTP_GIVENNAME"),
-                                       ("last_name", "HTTP_SN"),
-                                       ("email", "HTTP_MAIL")):
+    update_user = False
+    for attrib_name, meta_name in (("first_name", "HTTP_GIVENNAME"),
+                                   ("last_name", "HTTP_SN"),
+                                   ("email", "HTTP_MAIL")):
 
-            attrib_value = getattr(request.user, attrib_name)
-            meta_value = request.META.get(meta_name)
-            if meta_value != "(null)":
-                setattr(request.user, attrib_name, meta_value)
-                update = True
+        attrib_value = getattr(request.user, attrib_name)
+        meta_value = request.META.get(meta_name)
+        if meta_value != "(null)":
+            setattr(request.user, attrib_name, meta_value)
+            update_user = True
 
-        if request.user.password == "":
-            request.user.password = "(not used for federated logins)"
-            update = True
+    if request.user.password == "":
+        request.user.password = "(not used for federated logins)"
+        update_user = True
 
-        if update:
-            request.user.save()
+    if update_user:
+        request.user.save()
 
-        # User profile.
-        update_profile = False
-        default_profile = {'user': request.user,
-                           'creator': request.user,
-                           'display_name': '',
-                           'entitlements': '',
-                           'urlfilter': '.*',
-                           'expiration_date': None}
-        profile, created = request.user.profile.get_or_create(defaults=default_profile)
-        if created:
-            logger.info("Created user profile for user %s" % request.user.username)
+    # Get or create user profile.
+    update_profile = False
+    default_profile = {'user': request.user,
+                       'creator': request.user,
+                       'display_name': '',
+                       'entitlements': '',
+                       'urlfilter': '.*', # Logged in users can do everything.
+                       'expiration_date': None}
+    profile, created = request.user.profile.get_or_create(defaults=default_profile)
+    if created:
+        logger.info("Created user profile for user %s" % username)
 
-        entitlement_in = request.META.get('HTTP_ENTITLEMENT')
-        if entitlement_in:
-            profile.entitlements = entitlement_in
-            update_profile = True
+    entitlement_in = request.META.get('HTTP_ENTITLEMENT')
+    if entitlement_in != "(null)":
+        profile.entitlements = entitlement_in.replace(';', ' ')
+        update_profile = True
 
-        if not profile.display_name:
-            # FIXME: What was the idea with display_name again?
-            profile.display_name = '%s %s' % (request.user.first_name,
-                                              request.user.last_name)
-            update_profile = True
+    if not profile.display_name:
+        # FIXME: What was the idea with display_name again?
+        profile.display_name = '%s %s' % (request.user.first_name,
+                                          request.user.last_name)
+        update_profile = True
 
-        if update_profile:
-            profile.save()
+    if update_profile:
+        profile.save()
 
-        logger.debug("User %s profile: %s" % (request.user.username, repr(profile)))
-        logger.info("Accepted federated login for user %s from %s" % (request.user.username,
-                                                                      req_meta(request, "REMOTE_ADDR")))
+    # Successful login.
+    logger.debug("User %s profile: %s" % (username, repr(profile)))
+    logger.info("Accepted federated login for user %s from %s" % (username,
+                                                                  req_meta(request, "REMOTE_ADDR")))
 
-        # On a sucessful login, just do the redirect if one is requested
-        next = request.REQUEST.get("next", None)
-        if next is None:
-	    return render_to_response("share/login.html",{"user": request.user});
-        else:
-            return HttpResponseRedirect(next)
-
+    # On a sucessful login, just do the redirect if one is requested
+    next = request.REQUEST.get("next", None)
+    if next is None:
+        return render_to_response("share/login.html",{"user": request.user});
     else:
-        logger.warning("Failed federated login for user %s from %s" % (request.user.username,
-                                                                       req_meta(request, "REMOTE_ADDR")))
-
+        return HttpResponseRedirect(next)
