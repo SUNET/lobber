ba91487091004c6a8fe08b058dcbd470ec6c9597
Leif Johansson <leifj@sunet.se>
2010-04-11 12:29:41 +0200

    finish breakup of views.py

---
+++ src/lobber/share/keys.py
@@ -0,0 +1,71 @@
+import os
+from datetime import datetime as dt
+from hashlib import sha256
+from random import getrandbits
+
+from django.core.mail import send_mail
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render_to_response
+from django.core.servers.basehttp import FileWrapper
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
+from django.core.exceptions import ObjectDoesNotExist
+
+from lobber.settings import BASE_DIR, MEDIA_ROOT, LOGIN_URL, ANNOUNCE_URL, NORDUSHARE_URL, BASE_UI_URL, LOBBER_LOG_FILE
+from lobber.share.models import Torrent, Tag, UserProfile
+from forms import UploadForm, CreateKeyForm
+
+from lobber.resource import Resource
+import lobber.log
+logger = lobber.log.Logger("web", LOBBER_LOG_FILE)
+
+@login_required
+def api_keys(req):
+    """
+    GET ==> list keys
+    POST ==> create key
+    """
+    def _list(user):
+        lst = []
+        for p in UserProfile.objects.filter(creator=user):
+            if p.expiration_date and p.expiration_date <= dt.now():
+                continue
+            if not p.user.username.startswith('key:'):
+                continue
+            lst.append(p)
+        return lst
+
+    d = {'user': req.user}
+        
+    if req.method == 'GET':
+        d.update({'keys': _list(req.user)})
+        response = render_to_response('share/keys.html', d)
+    elif req.method == 'POST':
+        form = CreateKeyForm(req.POST)
+        if form.is_valid():
+            _create_key_user(req.user,
+                             form.cleaned_data['urlfilter'],
+                             form.cleaned_data['entitlements'],
+                             form.cleaned_data['expires'])
+            d.update({'keys': _list(req.user)})
+            response = render_to_response('share/keys.html', d)
+        else:
+            response = render_to_response('share/create_key.html',
+                                          {'form': CreateKeyForm(),
+                                           'user': req.user})
+    return response
+
+@login_required
+def api_key(req, inst):
+    """
+    GET ==> get key
+    DELETE ==> delete key
+    """
+    response = HttpResponse('NYI: not yet implemented')
+    return response
+
+def key_form(req):
+    return render_to_response('share/create_key.html',
+                              {'form': CreateKeyForm(),
+                               'user': req.user})
