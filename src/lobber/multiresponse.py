7cacd819553f497923e120718186000de929601a
Leif Johansson <leifj@sunet.se>
2010-04-25 23:43:25 +0200

    add date to the standard dict

--- src/lobber/multiresponse.py
+++ src/lobber/multiresponse.py
@@ -1,13 +1,11 @@
 import mimeparse
 import re
-import sys
 from django.conf import settings
 from django.shortcuts import render_to_response
-from django.template import Context
-from django.template import RequestContext
 from django.core.exceptions import ObjectDoesNotExist
 from lobber.settings import STOMP_HOST, STOMP_PORT, ORBITED_PREFIX, ANNOUNCE_URL
 from lobber.share.models import UserProfile
+from datetime import datetime
 
 default_suffix_mapping = {"\.htm(l?)$": "text/html",
                           "\.json$": "application/json",
@@ -15,41 +13,47 @@ default_suffix_mapping = {"\.htm(l?)$": "text/html",
                           "\.torrent$": "application/x-bittorrent"}
 
 def _accept_types(request, suffix):
-   for r in suffix.keys():
-      p = re.compile(r)
-      if p.search(request.path):
-         return suffix.get(r)
-   return None
+    for r in suffix.keys():
+        p = re.compile(r)
+        if p.search(request.path):
+            return suffix.get(r)
+    return None
+
+
+def timeAsrfc822 ( theTime ) :
+    import rfc822
+    return rfc822 . formatdate ( rfc822 . mktime_tz ( rfc822 . parsedate_tz ( theTime . strftime ( "%a, %d %b %Y %H:%M:%S" ) ) ) )
 
 def make_response_dict(request,d=None):
-   if d == None: 
-      d = {}
+    if d == None: 
+        d = {}
 
-   if request.user.is_authenticated():
-      d['user'] = request.user
-      profile = None
-      try:
-          profile = request.user.profile.get();
-      except ObjectDoesNotExist:
-          profile = UserProfile()
-      d['profile'] = profile
+    if request.user.is_authenticated():
+        d['user'] = request.user
+        profile = None
+        try:
+            profile = request.user.profile.get();
+        except ObjectDoesNotExist:
+            profile = UserProfile()
+            d['profile'] = profile
 
-   d['stomp_host'] = STOMP_HOST
-   d['stomp_port'] = STOMP_PORT
-   d['orbited_prefix'] = ORBITED_PREFIX
-   d['announce_url'] = ANNOUNCE_URL
+    d['stomp_host'] = STOMP_HOST
+    d['stomp_port'] = STOMP_PORT
+    d['orbited_prefix'] = ORBITED_PREFIX
+    d['announce_url'] = ANNOUNCE_URL
+    d['date'] = timeAsrfc822(datetime.now())
 
-   return d
+    return d
     
 def respond_to(request, template_mapping, dict, suffix_mapping=default_suffix_mapping):
-   accept = _accept_types(request, suffix_mapping)
-   if accept is None:
-      accept = (request.META['HTTP_ACCEPT'].split(','))[0]
-   content_type = mimeparse.best_match(template_mapping.keys(), accept)
-   template = template_mapping[content_type]
-   if callable(template):
-      response = template(make_response_dict(request,dict))
-   else:
-      response = render_to_response(template,make_response_dict(request,dict))
-      response['Content-Type'] = "%s; charset=%s" % (content_type, settings.DEFAULT_CHARSET)
-   return response
+    accept = _accept_types(request, suffix_mapping)
+    if accept is None:
+        accept = (request.META['HTTP_ACCEPT'].split(','))[0]
+    content_type = mimeparse.best_match(template_mapping.keys(), accept)
+    template = template_mapping[content_type]
+    if callable(template):
+        response = template(make_response_dict(request,dict))
+    else:
+        response = render_to_response(template,make_response_dict(request,dict))
+        response['Content-Type'] = "%s; charset=%s" % (content_type, settings.DEFAULT_CHARSET)
+    return response
