8b70130448d43d5e4cccef8cdc3a8747ffa80e8b
Leif Johansson <leifj@sunet.se>
2010-04-29 00:05:02 +0200

    ace cleanup

--- src/lobber/share/acl.py
+++ src/lobber/share/acl.py
@@ -2,6 +2,8 @@ import re
 from django.contrib.auth.decorators import login_required
 from django.utils.html import escape
 from lobber.share.models import Torrent
+from django.http import HttpResponse
+from django.core.exceptions import ObjectDoesNotExist
 
 @login_required
 def add_ace(req, tid, ace):
@@ -19,4 +21,10 @@ def add_ace(req, tid, ace):
 
 @login_required
 def remove_ace(req, tid, ace):
-    pass
+    try:
+        t = Torrent.objects.get(id=int(tid))
+    except ObjectDoesNotExist:
+        return HttpResponse("Torrent with id %d not found." % int(tid), status=404)
+
+    t.remove_ace(req.user,ace)
+    return HttpResponse()
--- src/lobber/share/models.py
+++ src/lobber/share/models.py
@@ -87,17 +87,40 @@ class Torrent(models.Model):
         #print 'DEBUG: perm %s denied for %s on %s for %s' % (perm, user, self, tag)
         return False
 
+    def get_ace(self, user):
+        return list(self.ace.split(' '))
+
+    def set_ace(self,user,aces):
+        self.ace = ' '.join(aces)
+
     def add_ace(self, user, ace):
         """Add ACE to ACL of self, prepended by 'user:<username>'
         where username is the name of USER.
 
         Return resulting ace on success.
         """
+        # XXX Should we really be doing this normalization? Seems to conflict with the spec of an ACE in lobber.org
         fullace = 'user:%s:%s' % (user.username, ace)
         if not acl_util.valid_ace_p(fullace):
             return None
-        self.acl += fullace
+        aces = self.get_ace(user)
+        aces.append(fullace)
+        self.set_ace(user, aces)
         return fullace
+    
+    def remove_ace(self,user,ace):
+        """
+        Remove ACE from ACL of self
+        """
+        # XXX Should we really be doing this normalization? Seems to conflict with the spec of an ACE in lobber.org
+        fullace = 'user:%s:%s' % (user.username, ace)
+        if not acl_util.valid_ace_p(fullace):
+            return None
+        aces = self.get_ace(user)
+        if fullace in aces:
+            aces.remove(ace)
+        return fullace
+        
 
     def readable_tags(self, user):
         return filter(lambda tag: self.authz_tag(user, "r", tag.name),
