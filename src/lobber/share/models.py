9e46255b0632ee02f48f36af339d9d138a0ca612
Linus Nordberg <linus@nordberg.se>
2010-04-29 00:39:39 +0200

    * src/lobber/share/models.py: Restore from stomping.

--- src/lobber/share/models.py
+++ src/lobber/share/models.py
@@ -5,7 +5,6 @@ from django.contrib.auth.models import User
 from lobber.settings import TORRENTS,BASE_URL
 import tagging
 from tagging.models import Tag
-import acl_util
 
 class Torrent(models.Model):
     name = models.CharField(max_length=256, blank=True)
@@ -87,6 +86,12 @@ class Torrent(models.Model):
         #print 'DEBUG: perm %s denied for %s on %s for %s' % (perm, user, self, tag)
         return False
 
+    def authz_acl(self, user, perm, ace):
+        "Does USER have PERM on torrent w.r.t. its ACL?"
+        if perm in 'rw':
+            return self.authz(user, perm)
+        return False                
+
     def get_ace(self, user):
         return list(self.ace.split(' '))
 
@@ -94,33 +99,24 @@ class Torrent(models.Model):
         self.ace = ' '.join(aces)
 
     def add_ace(self, user, ace):
-        """Add ACE to ACL of self, prepended by 'user:<username>'
-        where username is the name of USER.
-
-        Return resulting ace on success.
         """
-        # XXX Should we really be doing this normalization? Seems to conflict with the spec of an ACE in lobber.org
-        fullace = 'user:%s:%s' % (user.username, ace)
-        if not acl_util.valid_ace_p(fullace):
-            return None
-        aces = self.get_ace(user)
-        aces.append(fullace)
-        self.set_ace(user, aces)
-        return fullace
+        Add ACE to ACL of self.  Return True on success, False if
+        permission is denied.
+        """
+        if not self.authz_acl(user, 'w', ace):
+            return False
+        self.acl += ' ' + ace
+        return True
     
-    def remove_ace(self,user,ace):
+    def remove_ace(self, user, ace):
         """
-        Remove ACE from ACL of self
+        Remove ACE from ACL of self.
         """
-        # XXX Should we really be doing this normalization? Seems to conflict with the spec of an ACE in lobber.org
-        fullace = 'user:%s:%s' % (user.username, ace)
-        if not acl_util.valid_ace_p(fullace):
-            return None
         aces = self.get_ace(user)
-        if fullace in aces:
+        if ace in aces:
             aces.remove(ace)
-        return fullace
-        
+            return True
+        return False
 
     def readable_tags(self, user):
         return filter(lambda tag: self.authz_tag(user, "r", tag.name),
