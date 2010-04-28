859718ba171a49c173a9822a14bc95c6dd2fa7b2
Linus Nordberg <linus@nordberg.se>
2010-04-28 21:03:59 +0200

    * src/lobber/share/models.py (Torrent.authz_tag): Fix typo making read perm on tagging broken.

--- src/lobber/share/models.py
+++ src/lobber/share/models.py
@@ -71,7 +71,7 @@ class Torrent(models.Model):
         the torrent with TAG?
         """
         if perm == 'r':
-            return self.authz(user, tag)
+            return self.authz(user, perm)
         elif perm == 'w' or perm == 'd':
             if self.authz(user, 'w'):
                 if ':' in tag:       # Non-global tags are restricted.
