6da1ef0f915d7879a4a7a04dfb086c2f7583c506
Leif Johansson <leifj@sunet.se>
2010-05-10 23:18:50 +0200

    jbn leads the way

--- src/lobber/share/urls.py
+++ src/lobber/share/urls.py
@@ -1,24 +1,20 @@
 from django.conf.urls.defaults import *
-from lobber.share.links import send_link_mail, gimme_url_for_reading_torrent, gimme_url_for_reading_tag
-from lobber.share.torrent import TorrentView, remove_torrent, upload_jnlp, TorrentForm, exists, welcome, torrent_by_hashval
-from lobber.share.users import user_self
-from lobber.share.constraint import add_urlfilter, add_tagconstraint, remove_urlfilter, remove_tagconstraint
 
 urlpatterns = patterns('',
     # (regexp as a string, function without prefix as a string)
-    (r'^$', welcome),
+    (r'^$', "lobber.share.torrent.welcome"),
     ## RESTful API.
     # Torrents.
-    (r'^torrent/$',TorrentView),
-    (r'^torrent/all.rss$',TorrentView),
-    (r'^torrent/remove/(?P<tid>[0-9]+)$', remove_torrent),
-    (r'^torrent/sendlink/(?P<tid>[0-9]+)$',send_link_mail),
-    (r'^torrent/gufrt/(?P<tid>[0-9]+)$', gimme_url_for_reading_torrent),
-    (r'^torrent/add/$',TorrentForm),
-    (r'^torrent/jnlp/$',upload_jnlp),
-    (r'^torrent/(?P<inst>[0-9]+)\.([^\.]+)$', TorrentView),
-    (r'^torrent/(?P<inst>.+)\.torrent$', torrent_by_hashval),
-    (r'^torrent/exists/(?P<inst>.+)$', exists),
+    (r'^torrent/$',"lobber.share.torrent.TorrentView"),
+    (r'^torrent/all.rss$',"lobber.share.torrent.TorrentView"),
+    (r'^torrent/remove/(?P<tid>[0-9]+)\.([^\.]+)$', "lobber.share.torrent.remove_torrent"),
+    (r'^torrent/sendlink/(?P<tid>[0-9]+)$',"lobber.share.links.send_link_mail"),
+    (r'^torrent/gufrt/(?P<tid>[0-9]+)$', "lobber.share.links.gimme_url_for_reading_torrent"),
+    (r'^torrent/add/$',"lobber.share.torrent.TorrentForm"),
+    (r'^torrent/jnlp/$',"lobber.share.torrent.upload_jnlp"),
+    (r'^torrent/(?P<inst>[0-9]+)\.([^\.]+)$', "lobber.share.torrent.TorrentView"),
+    (r'^torrent/(?P<inst>.+)\.torrent$', "lobber.share.torrent.torrent_by_hashval"),
+    (r'^torrent/exists/(?P<inst>.+)$', "lobber.share.torrent.exists"),
     # Tagging.
     (r'^torrent/tags.json$', "lobber.share.tag.tag_usage"),
     (r'^torrent/tag/(?P<name>.+)\.([^\.]+)$', "lobber.share.tag.list_torrents_for_tag"),
@@ -28,12 +24,12 @@ urlpatterns = patterns('',
     (r'^torrent/(?P<tid>.+)/ace/add/(?P<ace>.+)$', "lobber.share.acl.add_ace"),
     (r'^torrent/(?P<tid>.+)/ace/remove/(?P<ace>.+)$', "lobber.share.acl.remove_ace"),
     # Constraints.
-    (r'^key/(?P<key>.+)/constraint/add/url/(?P<pattern>.+)$', add_urlfilter),
-    (r'^key/(?P<key>.+)/constraint/remove/url/(?P<pattern>.+)$', remove_urlfilter),
-    (r'^key/(?P<key>.+)/constraint/add/tag/(?P<tag>.+)$', add_tagconstraint),
-    (r'^key/(?P<key>.+)/constraint/remove/tag/(?P<tag>.+)$', remove_tagconstraint),
+    (r'^key/(?P<key>.+)/constraint/add/url/(?P<pattern>.+)$', "lobber.share.constraint.add_urlfilter"),
+    (r'^key/(?P<key>.+)/constraint/remove/url/(?P<pattern>.+)$', "lobber.share.constraint.remove_urlfilter"),
+    (r'^key/(?P<key>.+)/constraint/add/tag/(?P<tag>.+)$', "lobber.share.constraint.add_tagconstraint"),
+    (r'^key/(?P<key>.+)/constraint/remove/tag/(?P<tag>.+)$', "lobber.share.constraint.remove_tagconstraint"),
     # Keys
-    (r'^key\.[^\.]+$', "lobber.share.keys.keys"),
-    # Old stuff, pre API era.  FIXME: Clean up.
-    (r'^user/$', user_self), # Short for self.
+    (r'^key\.[^\.]+$',"lobber.share.keys.keys"),
+    # The user.
+    (r'^user/$',"lobber.share.users.user_self"),
     )
