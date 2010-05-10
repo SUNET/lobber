8de9d8cbf62e36ef7cbc5e3d6cfe128a5a09fe90
Leif Johansson <leifj@sunet.se>
2010-05-10 15:56:59 +0200

    some key cleanup

--- src/lobber/share/urls.py
+++ src/lobber/share/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls.defaults import *
-from lobber.share.keys import api_keys, key_form
+from lobber.share.keys import key, key_form
 from lobber.share.links import send_link_mail, gimme_url_for_reading_torrent, gimme_url_for_reading_tag
 from lobber.share.torrent import TorrentView, remove_torrent, upload_jnlp, TorrentForm, exists, welcome, torrent_by_hashval
 from lobber.share.users import user_self
@@ -23,20 +23,20 @@ urlpatterns = patterns('',
     (r'^torrent/(?P<inst>.+)\.torrent$', torrent_by_hashval),
     (r'^torrent/exists/(?P<inst>.+)$', exists),
     # Tagging.
-    (r'^torrent/tags.json$', tag_usage),
-    (r'^torrent/tag/(?P<name>.+)\.([^\.]+)$', list_torrents_for_tag),
-    (r'^torrent/(?P<tid>.+)/tags\.([^\.]+)$', tags),
-    (r'^tag/gufrt/(?P<tagstr>.+)', gimme_url_for_reading_tag),
+    (r'^torrent/tags.json$', "lobber.share.tag.tag_usage"),
+    (r'^torrent/tag/(?P<name>.+)\.([^\.]+)$', "lobber.share.tag.list_torrents_for_tag"),
+    (r'^torrent/(?P<tid>.+)/tags\.([^\.]+)$', "lobber.share.tag.tags"),
+    (r'^torrent/tag/gufrt/(?P<tagstr>.+)', "lobber.share.links.gimme_url_for_reading_tag"),
     # ACL handling.
-    (r'^torrent/(?P<tid>.+)/ace/add/(?P<ace>.+)$', add_ace),
-    (r'^torrent/(?P<tid>.+)/ace/remove/(?P<ace>.+)$', remove_ace),
+    (r'^torrent/(?P<tid>.+)/ace/add/(?P<ace>.+)$', "lobber.share.acl.add_ace"),
+    (r'^torrent/(?P<tid>.+)/ace/remove/(?P<ace>.+)$', "lobber.share.acl.remove_ace"),
     # Constraints.
-    (r'^key.html', key_form),
     (r'^key/(?P<key>.+)/constraint/add/url/(?P<pattern>.+)$', add_urlfilter),
     (r'^key/(?P<key>.+)/constraint/remove/url/(?P<pattern>.+)$', remove_urlfilter),
     (r'^key/(?P<key>.+)/constraint/add/tag/(?P<tag>.+)$', add_tagconstraint),
     (r'^key/(?P<key>.+)/constraint/remove/tag/(?P<tag>.+)$', remove_tagconstraint),
-    (r'^key/$', api_keys),
+    # Keys
+    (r'^key\.([^\.]+)$', key),
     # Old stuff, pre API era.  FIXME: Clean up.
     (r'^user/$', user_self), # Short for self.
     )
