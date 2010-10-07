d51ed1f35c2f8b333eac40a296af57618fcaa357
Linus Nordberg <linus@nordberg.se>
2010-10-08 00:53:58 +0200

    Purge oldish key users too.

--- src/scripts/expire.sh
+++ src/scripts/expire.sh
@@ -2,8 +2,12 @@
 
 # Expire torrents and key users which have passed their best date.
 
-# BUGS: Must be run from lobber/src (with argument '.' or `pwd`) when
+# BUGS:
+# - Must be run from lobber/src (with argument '.' or `pwd`) when
 # database is mysqlite.
+# - settings.BASE_DIR must contain an absolute path when database is
+# mysqlite, or removing User objects will fail with 'OperationalError:
+# no such table: share_datalocation'
 
 usage() {
     echo "usage: `basename $0`: base_dir"
