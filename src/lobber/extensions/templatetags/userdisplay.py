d0695db8e1209553d97de511cfae827fc0ed5cbf
Leif Johansson <leifj@sunet.se>
2010-10-08 11:07:30 +0200

    don't show key values

--- src/lobber/extensions/templatetags/userdisplay.py
+++ src/lobber/extensions/templatetags/userdisplay.py
@@ -10,15 +10,19 @@ from lobber.share.models import user_profile
 register = template.Library()
 
 def userdisplay(u):
-    display = user_profile(u).display_name
+    profile = user_profile(u)
+    display = profile.display_name
     if not display:
         r = None
-        try:
-            display, r = u.username.split('@')
-        except ValueError:
-            display = u.username
-        if r:
-            display = '%s (at %s)' % (display, r)
+        if u.username.startswith("key:") and profile.creator:
+            display = "a key created by %s" % userdisplay(profile.creator)
+        else:
+            try:
+                display, r = u.username.split('@')
+            except ValueError:
+                display = u.username
+            if r:
+                display = '%s (at %s)' % (display, r)
     if not display:
         display = "unknown user"
     return display
