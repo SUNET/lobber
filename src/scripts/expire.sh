a7af1575a4b210ffb4d29d524f6f36cd4f3e3a91
Linus Nordberg <linus@nordberg.se>
2010-10-08 11:08:32 +0200

    Point out expire.py from expire.sh.

--- src/scripts/expire.sh
+++ src/scripts/expire.sh
@@ -16,4 +16,4 @@ usage() {
 
 base_dir="$1"; [ -d "$base_dir" ] || usage
 shift
-${base_dir}/dev-django-admin.sh ${base_dir} shell $*
+${base_dir}/dev-django-admin.sh ${base_dir} shell < ${base_dir}/scripts/expire.py
