1c51003548bb4756e2ba158a92fc975a3410e08a
Leif Johansson <leifj@sunet.se>
2010-10-08 23:37:17 +0200

    debugging output for authz

--- src/lobber/share/models.py
+++ src/lobber/share/models.py
@@ -70,13 +70,11 @@ class Torrent(models.Model):
                 #print 'DEBUG: tag constraints not ok for %s with tag constraints %s, seeking #%c for torrrent %s' % (user, profile.tagconstraints, perm, self)
                 return False
 
-        logger.info("authz %s %s %s" % (self,user.username,perm))
         for ace in self.acl.split():
             ace_user, ace_perm = ace.split('#')
-            logger.info("\t -> %s %s" % (ace_user,ace_perm))
             #print 'DEBUG: ace_user: %s, ace_perm: %s, usernames: %s' % (ace_user, ace_perm, usernames)
             for username in usernames:
-                logger.info("\t\tentitlement: %s" % username)
+                logger.info("can %s do %s on %s by asserting =? %s " % (user,ace_perm,self,username,ace_user))
                 if ace_user == username and ace_perm == perm:
                     return True
                 if username.startswith('user:%s:' % user.username) and ace_perm == perm:
