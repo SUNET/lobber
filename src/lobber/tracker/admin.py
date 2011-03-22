907211bbdfc3131ba44e37c3888863ccd41c2342
Johan Lundberg <lundberg@nordu.net>
2011-03-22 17:47:27 +0100

    Better PeerInfo listing in admin interface.

--- src/lobber/tracker/admin.py
+++ src/lobber/tracker/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
 from lobber.tracker.models import PeerInfo
 
-admin.site.register(PeerInfo)
+class PeerInfoAdmin(admin.ModelAdmin):
+    list_display = ('peer_id', 'info_hash', 'address', 'state', 'last_seen')
+
+admin.site.register(PeerInfo, PeerInfoAdmin)
