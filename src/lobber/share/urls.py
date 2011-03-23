acb91bd8bd31bbcec6a9b25ae772689c0cafdef3
Leif Johansson <leifj@sunet.se>
2011-03-23 13:50:24 +0100

    cleanup some urls we don't use anymore

--- src/lobber/share/urls.py
+++ src/lobber/share/urls.py
@@ -1,21 +1,16 @@
 from django.conf.urls.defaults import patterns
 
 urlpatterns = patterns('',
-    # (regexp as a string, function without prefix as a string)
     (r'^$',"lobber.share.torrent.show"),
     (r'^all.rss$',"lobber.share.torrent.show"),
     (r'^all.json$',"lobber.share.torrent.show"),
     (r'^search(?:\.(?:[^\.]+))?$',"lobber.share.torrent.search"),
     (r'^remove/(?P<tid>[0-9]+)(?:\.([^\.]+))?$', "lobber.share.torrent.remove_torrent"),
-    #(r'^sendlink/(?P<tid>[0-9]+)$',"lobber.share.links.send_link_mail"),
-    #(r'^gufrt/(?P<tid>[0-9]+)$', "lobber.share.links.gimme_url_for_reading_torrent"),
     (r'^start-lobber-client.jnlp$',"lobber.share.torrent.upload_jnlp"),
     (r'^add(?:\.(?:[^\.]+))?$',"lobber.share.torrent.add_torrent"),
     (r'^(?P<inst>[0-9]+)(?:\.([^\.]+))?$', "lobber.share.torrent.show"),
-    #(r'^info/(?P<inst>[0-9]+)(?:\.([^\.]+))?$', "lobber.share.torrent.land"),
     (r'^(?P<inst>.+)\.torrent$', "lobber.share.torrent.torrent_by_hashval"),
     (r'^exists/(?P<inst>.+)$', "lobber.share.torrent.exists"),
-    #(r'^exists_new/(?P<inst>.+)$', "lobber.share.torrent.exists_new"),
     (r'^scrape/(?P<inst>[^\.]+)\.([^\.]+)$', "lobber.share.torrent.scrape"),
     (r'^scrapehash/(?P<hash>[^\.]+)(?:\.(?:[^\.]+))?$', "lobber.share.torrent.scrape_hash"),
     # Tagging.
