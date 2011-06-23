55237ee5725f1dd9db375c77e5cc1bde5efce774
Johan Lundberg <lundberg@nordu.net>
2011-06-23 15:17:19 +0200

    Added HttpResponseNotAuthorized.

--- src/lobber/common.py
+++ src/lobber/common.py
@@ -4,6 +4,20 @@ Created on Dec 3, 2010
 @author: leifj
 '''
 
+from django.http import HttpResponse
+
+class HttpResponseNotAuthorized(HttpResponse):
+    
+    def __init__(self, content=None):
+        self.status_code = 401
+        HttpResponse.__init__(self)
+        self['WWW-Authenticate'] = 'Key'
+        self.content = content
+        
+
+'''
+Can be replaced with .encode('hex')?
+'''
 def hexify(s):
     r = ''
     for n in range(0, len(s)):
