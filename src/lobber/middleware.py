f0cb4560105a631ce3a3dfb75b37ae780923b824
Johan Lundberg <lundberg@nordu.net>
2011-06-23 13:44:04 +0200

    Invalid keys and expired keys now return 403

--- src/lobber/middleware.py
+++ src/lobber/middleware.py
@@ -6,6 +6,9 @@ import re
 from django.contrib import auth
 from django.contrib.auth.models import User
 from django.core.exceptions import ImproperlyConfigured, ObjectDoesNotExist
+from django.http import HttpResponseForbidden
+from django.shortcuts import render_to_response
+from lobber.multiresponse import make_response_dict
 from lobber.settings import LOBBER_LOG_FILE
 import lobber.log
 logger = lobber.log.Logger("web", LOBBER_LOG_FILE)
@@ -34,18 +37,18 @@ class KeyMiddleware(object):
             user = User.objects.get(username=username)
         except ObjectDoesNotExist:
             logger.info("%s: user not found" % username)
-            return                      # Fail
+            return HttpResponseForbidden('Not a valid key.')
 
         try:
             profile = user.profile.get()
         except ObjectDoesNotExist:
             logger.error("%s: user profile not found" % username)
-            return                      # Fail.
+            return HttpResponseForbidden('Not a valid key.')
 
         if profile.expiration_date is not None and \
                profile.expiration_date <= dt.now():
             logger.info("%s: key expired" % secret)
-            return                      # Fail.
+            return HttpResponseForbidden('The key has expired.')
 
         filtermatch_flag = False
         cmd = request.path
@@ -57,7 +60,7 @@ class KeyMiddleware(object):
         if not filtermatch_flag:
             logger.info("%s: no match for url in filter (url=%s, filter=%s)"
                         % (username, cmd, urlfilter))
-            return                      # Fail.
+            return HttpResponseForbidden('Not a valid key for that URL.')
 
         # Authenticate and login user.
         auth_user = auth.authenticate(username=username, password=username)
@@ -68,10 +71,28 @@ class KeyMiddleware(object):
             request.session.flush()
         else:
             logger.debug("%s: failed authentication" % username)
-            return                  # Fail.
+            return HttpResponseForbidden('Not authorized.')
 
         # EPIC SUCCESS!!!  This is where we do anything needed for an
         # authenticated and logged in user.
+        
+    def process_exception(self, request, exception):
+        '''
+        Handles uncaught exceptions.
+        '''
+        logger.error("KeyMiddleware uncaught error: %s" % exception)
+        return render_to_response('handle_error.html', 
+                                  make_response_dict(request, 
+                                        {'exception': '%s' % exception}))
+                                        
+    def handle_exception(self, request, message):
+        '''
+        Create helpful error messages directed to the user.
+        '''
+        logger.error("KeyMiddleware handled error: %s" % message)
+        return render_to_response('handle_error.html', 
+                                  make_response_dict(request, 
+                                        {'message': message}))
         
     def clean_username(self, username, request):
         """
