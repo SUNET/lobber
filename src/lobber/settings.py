7171afcea557948f8082d9409509a8b963aaff1d
Johan Lundberg <lundberg@nordu.net>
2012-04-25 16:22:41 +0200

    Clean up.

--- src/lobber/settings.py
+++ src/lobber/settings.py
@@ -39,7 +39,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'lobber',                      # Or path to database file if using sqlite3.
         'USER': 'lobber',                      # Not used with sqlite3.
-        'PASSWORD': 'Mira1000',                  # Not used with sqlite3.
+        'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': 'localhost',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
